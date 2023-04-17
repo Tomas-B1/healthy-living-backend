89238b302fd5ba9064d3fe89e2b7ae65e6e18b7d
Tomas <t.bojkovski@gmail.com>
2023-04-17 11:16:25 -0400

    cors orgin fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 const corsConf = {
-  origin: "*",
+  origin: "https://healthyliving-tb.herokuapp.com/",
   methods: "GET,PUT,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204
