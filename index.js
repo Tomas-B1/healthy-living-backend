f456473b6344d7e6a1e478830b9d54893b1b4b80
Tomas <t.bojkovski@gmail.com>
2023-04-17 10:42:21 -0400

    fixes cors issue

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+const cors=require("cors");
+ const corsOptions ={
+       origin:'*', 
+       credentials:true, //access-control-allow-credentials:true
+        optionSuccessStatus:200,
+ }
+
+ app.use(cors(corsOptions)) 
+
+// app.use(cors());
 app.use(express.json());
 
 app.use("/user", userRoutes);
